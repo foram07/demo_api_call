a46f618c013214b663fc2d5bacc54232562aae32
Foram <developer.eww2@gmail.com>
2022-04-27 12:51:12 +0530

    Show loader till the data appears in List using FutureBuilder

--- lib/list/ListScreen.dart
+++ lib/list/ListScreen.dart
@@ -23,62 +23,75 @@ Future<List<User>> fetchData() async {
 }
 
 class ListingView extends State<ListScreen> {
-  List<User> user = [];
+  late Future<List<User>> user ;
 
   @override
   void initState() {
     super.initState();
 
-    fetchData().then((value) => {
-          setState(() => {user = value})
-        });
+   user= fetchData();
+       // .then((value) => {
+       //    setState(() => {user = value})
+       //  });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: user.length,
-          itemBuilder: (BuildContext context, int index) {
-            User userItem = user[index];
-            return GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(userItem)));
+        child: FutureBuilder(
+          future: user,
+          builder: (context, snapshot){
+           if (ConnectionState.active != null && !snapshot.hasData) {
+              return Center(child: Text("Loading..."));
+           }
+            if(ConnectionState.done !=null && snapshot.hasError){
+              return Center(child: Text('Something went wrong :('));
+            }
+            return ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (BuildContext context, int index) {
+                User userItem = (snapshot.data as List<User>)[index];
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(userItem)));
+                  },
+                  child: Container(
+                      height: 100,
+                      child: Card(
+                          child: Padding(
+                              padding: EdgeInsets.fromLTRB(20, 5, 5, 5),
+                              child: Container(
+                                  child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text("Name: ${userItem.name}"),
+                                            Text("Email: ${userItem.email}"),
+                                            Text("Company: ${userItem.company?.name}"),
+                                            Text("Contact Number: ${userItem.phone}")
+                                          ],
+                                        ),
+                                        Expanded(
+                                            child: Container(
+                                                alignment: Alignment.topRight,
+                                                child: Icon(
+                                                  Icons.favorite_border,
+                                                  color: Colors.red,
+                                                )))
+                                      ]))))),
+                );
               },
-              child: Container(
-                  height: 100,
-                  child: Card(
-                      child: Padding(
-                          padding: EdgeInsets.fromLTRB(20, 5, 5, 5),
-                          child: Container(
-                              child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text("Name: ${userItem.name}"),
-                                    Text("Email: ${userItem.email}"),
-                                    Text("Company: ${userItem.company?.name}"),
-                                    Text("Contact Number: ${userItem.phone}")
-                                  ],
-                                ),
-                                Expanded(
-                                    child: Container(
-                                        alignment: Alignment.topRight,
-                                        child: Icon(
-                                          Icons.favorite_border,
-                                          color: Colors.red,
-                                        )))
-                              ]))))),
+              itemCount: (snapshot.data as List<User>).length,
             );
-          },
-        ),
-      ),
-    );
+
+
+          }),
+        )
+      );
   }
 }
